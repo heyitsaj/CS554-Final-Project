2d74f0022e0370f94ff39ac5d91d19785169454b
jrailey13 <148562710+jrailey13@users.noreply.github.com>
2024-05-05 22:21:11 -0400

    Finishing up authentication, yet to be styled

--- code/react/src/components/Home.jsx
+++ code/react/src/components/Home.jsx
@@ -1,33 +1,72 @@
 import { Link, useNavigate } from 'react-router-dom';
 import './Home.css'; // Ensure to import the CSS file
+import { useEffect, useState } from 'react';
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth, signOutUser } from '../firebase/firebase-src';
 
 export default function Home() {
   const navigate = useNavigate();
+  const [user, setUser] = useState(null);
 
-  const onLoginOrSignUp = () => {
+  useEffect(() => {
+    const signedIn = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+    });
+    return () => signedIn();
+  }, []);
+
+  const onSignUpOrLogin = () => {
     navigate('/signUpOrLogin');
   }
 
+  const onSignOut = async () => {
+    try {
+      await signOutUser();
+      setUser(null);
+      navigate('/');
+    } catch(e) {
+      console.error(e);
+    }
+  }
+
+  // COMMENTED CODE WORKS BUT IS COMMENTED OUT WHILE WE ARE IN 
+  // DEVELOPMENT SO YOU DON'T HAVE TO SIGN IN EVERYTIME
   return (
     <div>
       <h1 style={{ textAlign: 'center' }}>Welcome to Pictogram!</h1>
+      {user ?
+       <button
+         onClick={onSignOut}
+         style={{ position: 'fixed', top: 30, right: 10, borderColor: 'black' }}
+       >
+         Sign Out
+       </button> :
       <button
-        onClick={onLoginOrSignUp}
+        onClick={onSignUpOrLogin}
         style={{ position: 'fixed', top: 30, right: 10, borderColor: 'black' }}
       >
         Sign Up/Login
-      </button>
+      </button>}
       <nav className='nav-bar'>
-        <Link className='nav-link' to='/'>
-          Home
-        </Link>
-        <Link className='nav-link' to='/SharedImages'>
+        <Link className='nav-link' to='/SharedImages' // onClick={user ? undefined : (e) => {
+          // e.preventDefault();
+          // alert("You must have an account to access this feature.")
+        //}}
+        >
           Image Sharing
         </Link>
-        <Link className='nav-link' to='/CreatedImages'>
+        <Link className='nav-link' to='/CreatedImages' // onClick={user ? undefined : (e) => {
+          // e.preventDefault();
+          // alert("You must have an account to access this feature.")
+        //}}
+        >
           Image Creation
         </Link>
-        <Link className='nav-link' to='/Leaderboard'>
+        <Link className='nav-link' to='/Leaderboard' // onClick={user ? undefined : (e) => {
+          //e.preventDefault();
+          //alert("You must have an account to access this feature.")
+        //}}
+        >
           Leaderboard
         </Link>
         <Link className='nav-link' to='/AboutUs'>
--- code/react/src/components/SignUpOrLogin.jsx
+++ code/react/src/components/SignUpOrLogin.jsx
@@ -2,17 +2,20 @@ import React, { useState } from 'react';
 import './App.css';
 import { signInUser, signUpUser } from '../firebase/firebase-src';
 import { AuthErrorCodes } from 'firebase/auth';
+import { useNavigate } from 'react-router-dom';
 
 function SignUpOrLogin() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [error, setError] = useState('');
 
+    const navigate = useNavigate();
 
     const onLogin = async () => {
         try {
             await signInUser(email, password);
             setError('');
+            navigate('/');
         } catch (e) {
             if (e.code === 'auth/invalid-email') {
                 setError("Please enter a valid email address.");
@@ -20,13 +23,13 @@ function SignUpOrLogin() {
             else if (e.code === AuthErrorCodes.EMAIL_EXISTS) {
                 setError("Email already exists. Please try again.");
             }
-            else if (e.code === AuthErrorCodes.INVALID_PASSWORD) {
+            else if (e.code === 'auth/weak-password') {
                 setError("Password must be longer than six characters.");
             }
-            else if (e.code === AuthErrorCodes.MISSING_APP_CREDENTIAL) {
+            else if (e.code === 'auth/missing-email' || e.code === 'auth/missing-password') {
                 setError("Please enter both an email and password.");
             }
-            else if (e.code === AuthErrorCodes.INVALID_APP_CREDENTIAL) {
+            else if (e.code === 'auth/invalid-credential') {
                 setError("Invalid email or password. Please try again.");
             }
             else {
@@ -39,6 +42,7 @@ function SignUpOrLogin() {
         try {
             await signUpUser(email, password);
             setError('');
+            alert('Welcome to Pictogram! Please login with your credentials to begin using the app.');
         } catch (e) {
             if (e.code === 'auth/invalid-email') {
                 setError("Please enter a valid email address.");
@@ -46,13 +50,13 @@ function SignUpOrLogin() {
             else if (e.code === AuthErrorCodes.EMAIL_EXISTS) {
                 setError("Email already exists. Please try again.");
             }
-            else if (e.code === AuthErrorCodes.INVALID_PASSWORD) {
+            else if (e.code === 'auth/weak-password') {
                 setError("Password must be longer than six characters.");
             }
-            else if (e.code === AuthErrorCodes.MISSING_APP_CREDENTIAL) {
+            else if (e.code === 'auth/missing-email' || e.code === 'auth/missing-password') {
                 setError("Please enter both an email and password.");
             }
-            else if (e.code === AuthErrorCodes.INVALID_APP_CREDENTIAL) {
+            else if (e.code === 'auth/invalid-credential') {
                 setError("Invalid email or password. Please try again.");
             }
             else {
