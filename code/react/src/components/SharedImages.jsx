e1ea8873f5d9a6bd2e68d3f5081901e4eee746d3
Andrew McCauley <ajman888@gmail.com>
2024-05-08 01:51:13 -0400

    Fixed/Merged Conflicts

--- code/react/src/components/SharedImages.jsx
+++ code/react/src/components/SharedImages.jsx
@@ -69,15 +69,10 @@ export default function SharedImages() {
       <div>
         <Navigation />
         <h1>Welcome to the Shared Images Page!</h1>
-<<<<<<< Updated upstream
-        {user && <h3>Here you can upload images and edit images if you are signed up and logged in.</h3>}
-        {user && <button className='button' onClick={() => setShowAddForm(!showAddForm)}>Upload Shared Image</button>}
-        {user && showAddForm && <Add type='sharedImage' closeAddFormState={closeAddFormState} />}
-=======
-        <h3>Here you can upload images and edit images.</h3>
+        <h3>Here you can upload images and edit images if you are signed up and logged in.</h3>
         {user ? <button className='button' onClick={() => setShowAddForm(!showAddForm)}>Upload Shared Image</button> : <h3>You must be signed in to upload or edit images!</h3>}
-        {showAddForm && <Add type='sharedImage' closeAddFormState={closeAddFormState} />}
->>>>>>> Stashed changes
+        {user && showAddForm && <Add type='sharedImage' closeAddFormState={closeAddFormState} />}
+
         <br /><br />
         {sharedImages && sharedImages.map((sharedImage) => (
           <div className='card' key={sharedImage._id}>
@@ -85,19 +80,12 @@ export default function SharedImages() {
               <img src={sharedImage.image} alt="Shared" width="500" />
               <p>Created on {new Date(sharedImage.dateFormed).toLocaleDateString()} at {new Date(sharedImage.dateFormed).getHours().toString()}:{new Date(sharedImage.dateFormed).getMinutes().toString()} by {userHelper.renderUserEmail(users, sharedImage.userId)}</p>
               <p>Description: {sharedImage.description}</p>
-<<<<<<< Updated upstream
-              {user && <button className='button' onClick={() => handleOpenEditModal(sharedImage)}>Edit</button> }
-              {user && user.uid === sharedImage.userId ? <button className='button' onClick={() => handleOpenDeleteModal(sharedImage)}>Delete</button> : <></>}
-=======
               {user ? 
               <>
                 <button className='button' onClick={() => handleOpenEditModal(sharedImage)}>Edit</button>
                 {user.uid === sharedImage.userId ? <button className='button' onClick={() => handleOpenDeleteModal(sharedImage)}>Delete</button> : <></>}
               </>
               : <></>}
-              
-              
->>>>>>> Stashed changes
             </div>
           </div>
         ))}
